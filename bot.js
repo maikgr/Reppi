1db4aa28bc104367a9b69115972853c268f00209
Mike <neg8520@gmail.com>
2018-03-25 21:40:27 +0800

    Improved bot to generate Image first

--- bot.js
+++ bot.js
@@ -1,50 +1,54 @@
 var countFiles = require('count-files')
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
 var standardGacha = require('./standardGacha.js');
 var memeFileName;
 
-
-client.login('NDI0ODA4NDA2NDcwMzYxMDg4.DY-RqQ.X32wbXehoOMzDurLGal26z_l-GM');
+client.login('NDI2NzU2NjQ4MTY2NjIxMTg0.DZanng.dlDgwQY3Vx3V1mIMplUlDZUptkk');
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
 });
 
-function getRandomInt(min, max){
-    var intMin = parseInt(min);
-    var intMax = parseInt(max);
-    return (Math.floor(Math.random() * (intMax-intMin)) + intMin);
+function getRandomInt(min, max) {
+  var intMin = parseInt(min);
+  var intMax = parseInt(max);
+  return (Math.floor(Math.random() * (intMax - intMin)) + intMin);
 }
 
-countFiles('images/random', function (err, results) {
+countFiles('images/random', function(err, results) {
   connectClient(results.files);
 });
 
-function connectClient(memeFileTotal){
+function connectClient(memeFileTotal) {
   client.on('message', msg => {
-    var channel = client.channels.get('378748690095013919');
+    var channel = client.channels.get('424485532794355732');
     //378748690095013919 for Repulsion
     if (msg.content === '!gacha') {
-      if(msg.channel.name !== 'bot-spam'){
+      if (msg.channel.name !== 'bot-spam') {
         msg.reply("Does this looks like " + channel + " channel to you? If yes, I suggest you to get your eyesight checked.")
       } else {
 
         memeChance = Math.floor(Math.random() * 100) / 100;
         console.log("memeChance : " + memeChance);
-
         memeFileName = getRandomInt(1, memeFileTotal + 1);
 
-        if(memeChance < 0.07){
+        if (memeChance < 0.07) {
           msg.reply("", {
-          file: "./images/random/" + memeFileName + ".JPG"
+            file: "./images/random/" + memeFileName + ".JPG"
           });
         } else {
-          standardGacha.gachaStart();
-          msg.reply("", {
-          file: "output.jpg"
-          });
+          start();
+
+          async function start(){
+            let done = await standardGacha.gachaStart();
+            if (done == 'done'){
+              msg.reply("", {
+                file: "output.jpg"
+              });
+            }
+          }
+
         }
       }
     }
--- standardGacha.js
+++ standardGacha.js
@@ -16,49 +16,49 @@ var getSRankText = "";
 var avatarRank = "";
 var countingFiles = "";
 
-
-
-var gachaStart = function(){
+async function gachaStart() {
   getSRank = 0;
   guaranteed = 0;
   resultArray = [];
   chatBox = "";
-
-  for (var i = 0; i < 9; i++){
+  for (var i = 0; i < 9; i++) {
     gachaDraw();
   }
-
-  if (guaranteed == 9){
+  if (guaranteed == 9) {
     drawValkyrieGuaranteed()
   } else {
     gachaDraw();
   }
-  getCharacterAvatar();
+  let counted = await fileCount();
+  let imageGenerated = await generateImage(counted);
+  return new Promise(function(resolve, reject) {
+    resolve('done');
+  });
+
 };
 
-function gachaDraw(){
+function gachaDraw() {
   gachaRate = Math.floor(Math.random() * 100) / 100;
-  if (gachaRate < 0.2){
-      drawValkyrie()
-  }
-  else{
-      drawItem();
-      guaranteed++;
+  if (gachaRate < 0.2) {
+    drawValkyrie()
+  } else {
+    drawItem();
+    guaranteed++;
   }
 }
 
-function drawItem(){
+function drawItem() {
   itemNumber = 0
-  itemNumber = getRandomInt(1,10);
+  itemNumber = getRandomInt(1, 10);
   resultArray.push("items/" + itemNumber + ".PNG");
 }
 
-function drawValkyrie(){
+function drawValkyrie() {
   var rate = Math.floor(Math.random() * 100) / 100;
-  if (rate < 0.073){
+  if (rate < 0.073) {
     drawValkyrieSRank()
     getSRank++;
-  } else if (rate < 0.73){
+  } else if (rate < 0.73) {
     drawValkyrieARank()
   } else {
     drawValkyrieBRank()
@@ -66,9 +66,9 @@ function drawValkyrie(){
   }
 }
 
-function drawValkyrieGuaranteed(){
+function drawValkyrieGuaranteed() {
   var rate = Math.floor(Math.random() * 100) / 100;
-  if (rate < 0.1){
+  if (rate < 0.1) {
     drawValkyrieSRank()
     getSRank++;
   } else {
@@ -76,68 +76,73 @@ function drawValkyrieGuaranteed(){
   }
 }
 
-function drawValkyrieSRank(){
+function drawValkyrieSRank() {
   var rand = getRandomInt(0, standard.valk.s.length);
   resultArray.push("valkyrie/" + standard.valk.s[rand]);
 }
 
-function drawValkyrieARank(){
+function drawValkyrieARank() {
   var rand = getRandomInt(0, standard.valk.a.length);
   resultArray.push("valkyrie/" + standard.valk.a[rand]);
 }
 
-function drawValkyrieBRank(){
+function drawValkyrieBRank() {
   var rand = getRandomInt(0, standard.valk.b.length);
   resultArray.push("valkyrie/" + standard.valk.b[rand]);
 }
 
-function getRandomInt(min, max){
-    var intMin = parseInt(min);
-    var intMax = parseInt(max);
-    return (Math.floor(Math.random() * (intMax-intMin)) + intMin);
+function getRandomInt(min, max) {
+  var intMin = parseInt(min);
+  var intMax = parseInt(max);
+  return (Math.floor(Math.random() * (intMax - intMin)) + intMin);
 }
 
+function generateImage(valkyrieChatNumber) {
+  chatBoxNumber = getRandomInt(1, valkyrieChatNumber + 1);
+  return new Promise(function(resolve, reject) {
+    images("input.jpg")
+      .size(1280, 950)
+      .draw(images("images/" + resultArray[0]).size(160, 160), 174, 386)
+      .draw(images("images/" + resultArray[1]).size(160, 160), 372, 386)
+      .draw(images("images/" + resultArray[2]).size(160, 160), 567, 386)
+      .draw(images("images/" + resultArray[3]).size(160, 160), 760, 386)
+      .draw(images("images/" + resultArray[4]).size(160, 160), 950, 386)
+      .draw(images("images/" + resultArray[5]).size(160, 160), 174, 576)
+      .draw(images("images/" + resultArray[6]).size(160, 160), 372, 576)
+      .draw(images("images/" + resultArray[7]).size(160, 160), 567, 576)
+      .draw(images("images/" + resultArray[8]).size(160, 160), 760, 576)
+      .draw(images("images/" + resultArray[9]).size(160, 160), 950, 576)
+      .draw(images("images/chat/" + chatBoxChar + "/" + getSRankText + "/" + chatBoxNumber + ".PNG").size(989, 276), 0, 680)
+      .draw(images("images/chat/" + chatBoxChar + "/" + avatarRank).size(242, 276), 0, 680)
+      .save("output.jpg", {
+        quality: 50
+      });
+    if (images.getUsedMemory() != 0) {
+      resolve(images.getUsedMemory());
+    }
+  });
+}
 
-function getCharacterAvatar(){
+function fileCount() {
   resultArray.sort().reverse();
   var rand = getRandomInt(0, chatGacha.character.length);
   chatBoxChar = chatGacha.character[rand];
   getSRankText = "";
   avatarRank = "";
-  if (getSRank > 0){
+  if (getSRank > 0) {
     getSRankText = "srank";
     avatarRank = "Savatar.PNG";
   } else {
     getSRankText = "nosrank";
     avatarRank = "avatar.PNG";
   }
-
-  countFiles('images/chat/' + chatBoxChar + '/' + getSRankText, function (err, results) {
-    generateImage(results.files);
-  });
-
-}
-
-function generateImage(valkyrieChatNumber){
-  chatBoxNumber = getRandomInt(1,valkyrieChatNumber + 1);
-
-  images("input.jpg")
-    .size(1280,950)
-    .draw(images("images/" + resultArray[0]) .size(160,160), 174, 386)
-    .draw(images("images/" + resultArray[1]) .size(160,160), 372, 386)
-    .draw(images("images/" + resultArray[2]) .size(160,160), 567, 386)
-    .draw(images("images/" + resultArray[3]) .size(160,160), 760, 386)
-    .draw(images("images/" + resultArray[4]) .size(160,160), 950, 386)
-    .draw(images("images/" + resultArray[5]) .size(160,160), 174, 576)
-    .draw(images("images/" + resultArray[6]) .size(160,160), 372, 576)
-    .draw(images("images/" + resultArray[7]) .size(160,160), 567, 576)
-    .draw(images("images/" + resultArray[8]) .size(160,160), 760, 576)
-    .draw(images("images/" + resultArray[9]) .size(160,160), 950, 576)
-    .draw(images("images/chat/" + chatBoxChar + "/" + getSRankText + "/" + chatBoxNumber + ".PNG") .size(989,276), 0,680)
-    .draw(images("images/chat/" + chatBoxChar + "/" + avatarRank) .size(242,276), 0,680)
-    .save("output.jpg", {
-      quality :50
+  return new Promise(function(resolve, reject) {
+    countFiles('images/chat/' + chatBoxChar + '/' + getSRankText, function(err, results) {
+      if (results.files != 0) {
+        resolve(results.files);
+      }
     });
+  })
 }
 
 exports.gachaStart = gachaStart;

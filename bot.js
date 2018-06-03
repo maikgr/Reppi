bf174e8e4756eecca8f74646abbbd43369064cfe
Mike <neg8520@gmail.com>
2018-06-03 18:36:56 +0800

    Added stigmata search

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@ const standardGacha = require('./standardGacha.js');
 
 const equipmentDatabase = require('./equipmentdb.js');
 const weaponDB = require('./equipmentdb2.json').weapon;
+const stigmataDB = require('./equipmentdb2.json').stigmata;
 
 let memeFileName;
 
@@ -1556,31 +1557,31 @@ function connectClient(memeFileTotal) {
       }).then(async (newMessage) => {
         const filter = (reaction, user) =>
           user.id === msg.author.id &&
-        reaction.emoji.name === '◀' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '1⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '2⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '3⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '4⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '5⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '6⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '7⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '8⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '9⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '▶' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '❌';
+          reaction.emoji.name === '◀' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '1⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '2⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '3⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '4⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '5⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '6⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '7⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '8⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '9⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '▶' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '❌';
         const collector = newMessage.createReactionCollector(filter, { time: 30000 });
-        newMessage.delete(30000).catch(() => {});
+        newMessage.delete(30000).catch(() => { });
         collector.once('collect', async (reaction) => {
           const chosen = reaction.emoji.name;
           if (chosen === '◀') {
@@ -1723,24 +1724,24 @@ function connectClient(memeFileTotal) {
         });
 
         if (page > 0) {
-          await newMessage.react('◀').catch(() => {});
+          await newMessage.react('◀').catch(() => { });
         }
 
         if (stigmataArrayNameTemp.length > 9) {
-          await newMessage.react('▶').catch(() => {});
+          await newMessage.react('▶').catch(() => { });
         }
 
         if (stigmataArrayNameTemp.length <= 9) {
           for (let i = 0; i < stigmataArrayNameTemp.length; i++) {
-            await newMessage.react(reactionNumberArray[i]).catch(() => {});
+            await newMessage.react(reactionNumberArray[i]).catch(() => { });
           }
         } else {
           for (let i = 0; i < 9; i++) {
-            await newMessage.react(reactionNumberArray[i]).catch(() => {});
+            await newMessage.react(reactionNumberArray[i]).catch(() => { });
           }
         }
 
-        await newMessage.react('❌').catch(() => {});
+        await newMessage.react('❌').catch(() => { });
 
       });
 
@@ -1771,19 +1772,19 @@ function connectClient(memeFileTotal) {
       }).then(async (newMessage) => {
         const filter = (reaction, user) =>
           user.id === msg.author.id &&
-        reaction.emoji.name === '1⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '2⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '3⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '4⃣' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '▶' ||
-        user.id === msg.author.id &&
-        reaction.emoji.name === '❌';
+          reaction.emoji.name === '1⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '2⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '3⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '4⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '▶' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '❌';
         const collector = newMessage.createReactionCollector(filter, { time: 30000 });
-        newMessage.delete(30000).catch(() => {});
+        newMessage.delete(30000).catch(() => { });
         collector.once('collect', async (reaction) => {
           const chosen = reaction.emoji.name;
           if (chosen === '1⃣') {
@@ -1803,17 +1804,375 @@ function connectClient(memeFileTotal) {
           }
           collector.stop();
         });
-        await newMessage.react('1⃣').catch(() => {});
-        await newMessage.react('2⃣').catch(() => {});
-        await newMessage.react('3⃣').catch(() => {});
-        await newMessage.react('4⃣').catch(() => {});
-        await newMessage.react('❌').catch(() => {});
+        await newMessage.react('1⃣').catch(() => { });
+        await newMessage.react('2⃣').catch(() => { });
+        await newMessage.react('3⃣').catch(() => { });
+        await newMessage.react('4⃣').catch(() => { });
+        await newMessage.react('❌').catch(() => { });
       });
+    }
+
+    async function stigmataSearchMenu(indexes) {
+      const result = [];
+      const reactionNumberArray = ['1⃣', '2⃣', '3⃣', '4⃣', '5⃣', '6⃣', '7⃣', '8⃣', '9⃣'];
+      for (let i = 0; i < indexes.length; i++) {
+        result.push(stigmataDB[indexes[i]]);
+      }
+
+      let embedStigmataName = '';
+      let beginningNumber = 0;
+      const resultIndex = [];
+      for (let i = 0; i < result.length; i++) {
+        if (embedStigmataName === '') {
+          beginningNumber += 1;
+          embedStigmataName += `${beginningNumber}. ${result[i].name}\n`;
+          resultIndex.push(indexes[i]);
+        } else if (result[i].name !== result[i - 1].name) {
+          beginningNumber += 1;
+          embedStigmataName += `${beginningNumber}. ${result[i].name}\n`;
+          resultIndex.push(indexes[i]);
+        }
+
+      }
+
+      const embed = {
+        author: {
+          name: 'Stigmata search result',
+        },
+        color: 6332693,
+        title: 'Select a stigmata',
+        footer: {
+          icon_url: 'attachment://attentionicon.PNG',
+          text: 'Please press the number below (reaction) to select',
+        },
+        fields: [{
+          name: '----------------',
+          value: `${embedStigmataName}`,
+        }],
+      };
+
+      msg.reply({
+        embed,
+        files: [{
+          attachment: 'images/attentionicon.PNG',
+          name: 'attentionicon.PNG',
+        }],
+      }).then(async (newMessage) => {
+        const filter = (reaction, user) =>
+          user.id === msg.author.id &&
+          reaction.emoji.name === '1⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '2⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '3⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '4⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '5⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '6⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '7⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '8⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '9⃣' ||
+          user.id === msg.author.id &&
+          reaction.emoji.name === '❌';
+        const collector = newMessage.createReactionCollector(filter, { time: 30000 });
+        newMessage.delete(30000).catch(() => { });
+        collector.once('collect', async (reaction) => {
+          const chosen = reaction.emoji.name;
+
+          for (let i = 0; i < beginningNumber; i++) {
+            const emoji = reactionNumberArray[i];
+            if (chosen === emoji) {
+              newMessage.delete();
+              if (result.length < 1) {
+                msg.reply('please do not add reaction by yourself.');
+              } else if (stigmataDB[resultIndex[i]].name === stigmataDB[resultIndex[i] + 1].name) {
+                await msg.channel.send({
+                  embed: {
+                    author: {
+                      icon_url: 'attachment://top.PNG',
+                      name: stigmataDB[resultIndex[i]].name,
+                    },
+                    color: 16748921,
+                    thumbnail: {
+                      url: stigmataDB[resultIndex[i]].image,
+                    },
+                    fields: [{
+                      name: 'Effect',
+                      value: stigmataDB[resultIndex[i]].skill,
+                    }],
+                  },
+                  files: [{
+                    attachment: 'images/equipment/stigmata/top.PNG',
+                    name: 'top.PNG',
+                  }],
+                });
+                await msg.channel.send({
+                  embed: {
+                    author: {
+                      icon_url: 'attachment://middle.PNG',
+                      name: stigmataDB[resultIndex[i] + 1].name,
+                    },
+                    color: 10070783,
+                    thumbnail: {
+                      url: stigmataDB[resultIndex[i] + 1].image,
+                    },
+                    fields: [{
+                      name: 'Effect',
+                      value: stigmataDB[resultIndex[i] + 1].skill,
+                    }],
+                  },
+                  files: [{
+                    attachment: 'images/equipment/stigmata/middle.PNG',
+                    name: 'middle.PNG',
+                  }],
+                });
+                await msg.channel.send({
+                  embed: {
+                    author: {
+                      icon_url: 'attachment://bottom.PNG',
+                      name: stigmataDB[resultIndex[i] + 2].name,
+                    },
+                    color: 11716965,
+                    thumbnail: {
+                      url: stigmataDB[resultIndex[i] + 2].image,
+                    },
+                    fields: [{
+                      name: 'Effect',
+                      value: stigmataDB[resultIndex[i] + 2].skill,
+                    }],
+                  },
+                  files: [{
+                    attachment: 'images/equipment/stigmata/bottom.PNG',
+                    name: 'bottom.PNG',
+                  }],
+                });
+                msg.channel.send({
+                  embed: {
+                    author: {
+                      name: stigmataDB[resultIndex[i] + 3].name,
+                    },
+                    color: 16777215,
+                    footer: {
+                      icon_url: 'attachment://attentionicon.PNG',
+                      text: 'All information from http://houkai3rd.arthobbylab.com/',
+                    },
+                    fields: [{
+                      name: '2 Sets',
+                      value: stigmataDB[resultIndex[i] + 3].set1,
+                    }, {
+                      name: '3 Sets',
+                      value: stigmataDB[resultIndex[i] + 3].set2,
+                    }],
+                  },
+                  files: [{
+                    attachment: 'images/attentionicon.PNG',
+                    name: 'attentionicon.PNG',
+                  }],
+                });
+              } else {
+                let authorIcon = '';
+                let stigmataColor = '';
+                if (stigmataDB[resultIndex[i]].type === 'top') {
+                  authorIcon = 'top.PNG';
+                  stigmataColor = 16748921;
+                }
+                if (stigmataDB[resultIndex[i]].type === 'mid') {
+                  authorIcon = 'middle.PNG';
+                  stigmataColor = 10070783;
+                }
+                if (stigmataDB[resultIndex[i]].type === 'bottom') {
+                  authorIcon = 'bottom.PNG';
+                  stigmataColor = 11716965;
+                }
+                await msg.channel.send({
+                  embed: {
+                    author: {
+                      icon_url: `attachment://${authorIcon}`,
+                      name: stigmataDB[resultIndex[i]].name,
+                    },
+                    color: stigmataColor,
+                    thumbnail: {
+                      url: stigmataDB[resultIndex[i]].image,
+                    },
+                    fields: [{
+                      name: 'Effect',
+                      value: stigmataDB[resultIndex[i]].skill,
+                    }],
+                  },
+                  files: [{
+                    attachment: `images/equipment/stigmata/${authorIcon}`,
+                    name: authorIcon,
+                  }],
+                });
+              }
+            }
+          }
+
+          if (chosen === '❌') {
+            newMessage.delete();
+          }
+          collector.stop();
+        });
+
+        for (let i = 0; i < beginningNumber; i++) {
+          await newMessage.react(reactionNumberArray[i]).catch(() => { });
+        }
+
+        await newMessage.react('❌').catch(() => { });
+
+      });
+
+    }
+
+    async function stigmataSearch() {
+      const query = msg.content.replace('!stigmata ', '');
+      if (query.length <= 2) {
+        msg.reply('Please search with atleast 3 characters!');
+      } else {
+        const queryConvertedEscapes = query.replace(/[^a-zA-Z0-9]/g, ' ');
+        const queryConvertedSpace = queryConvertedEscapes.replace(/ /g, ')(?=.*');
+        const queryConvertedBeginningEnd = `${queryConvertedSpace.replace(/^/g, '(?=.*')})`;
+
+        const regex = new RegExp(queryConvertedBeginningEnd, 'i');
+        const indexes = [];
+        for (let i = 0; i < stigmataDB.length; i++) {
+          if (JSON.stringify(stigmataDB[i].name).match(regex)) {
+            indexes.push(i);
+          }
+        }
+        const result = [];
+        for (let i = 0; i < indexes.length; i++) {
+          result.push(stigmataDB[indexes[i]]);
+        }
+
+        if (result.length < 5 && result.length > 3) {
+          if (indexes[0] + 1 === indexes[1]) {
+            for (let i = 0; i < 3; i++) {
+              let authorIcon = '';
+              let stigmataColor = '';
+              if (i === 0) {
+                authorIcon = 'top.PNG';
+                stigmataColor = 16748921;
+              }
+              if (i === 1) {
+                authorIcon = 'middle.PNG';
+                stigmataColor = 10070783;
+              }
+              if (i === 2) {
+                authorIcon = 'bottom.PNG';
+                stigmataColor = 11716965;
+              }
+              if (stigmataDB[indexes[i]].name !== '-') {
+                await msg.channel.send({
+                  embed: {
+                    author: {
+                      icon_url: `attachment://${authorIcon}`,
+                      name: stigmataDB[indexes[i]].name,
+                    },
+                    color: stigmataColor,
+                    thumbnail: {
+                      url: stigmataDB[indexes[i]].image,
+                    },
+                    fields: [{
+                      name: 'Effect',
+                      value: stigmataDB[indexes[i]].skill,
+                    }],
+                  },
+                  files: [{
+                    attachment: `images/equipment/stigmata/${authorIcon}`,
+                    name: authorIcon,
+                  }],
+                });
+              }
+            }
+
+            if (stigmataDB[indexes[3]].name !== '-') {
+              msg.channel.send({
+                embed: {
+                  author: {
+                    name: stigmataDB[indexes[3]].name,
+                  },
+                  color: 16777215,
+                  footer: {
+                    icon_url: 'attachment://attentionicon.PNG',
+                    text: 'All information from http://houkai3rd.arthobbylab.com/',
+                  },
+                  fields: [{
+                    name: '2 Sets',
+                    value: stigmataDB[indexes[3]].set1,
+                  }, {
+                    name: '3 Sets',
+                    value: stigmataDB[indexes[3]].set2,
+                  }],
+                },
+                files: [{
+                  attachment: 'images/attentionicon.PNG',
+                  name: 'attentionicon.PNG',
+                }],
+              });
+            }
+          } else {
+            stigmataSearchMenu(indexes);
+          }
+        } if (result.length > 4 && result.length < 37) {
+          stigmataSearchMenu(indexes);
+        } else if (result.length > 36) {
+          msg.reply('Too many results! Be specific with your search!');
+        } else if (result.length > 0 && result.length < 2) {
+          let authorIcon = '';
+          let stigmataColor = '';
+          if (stigmataDB[indexes[0]].type === 'top') {
+            authorIcon = 'top.PNG';
+            stigmataColor = 16748921;
+          }
+          if (stigmataDB[indexes[0]].type === 'mid') {
+            authorIcon = 'middle.PNG';
+            stigmataColor = 10070783;
+          }
+          if (stigmataDB[indexes[0]].type === 'bottom') {
+            authorIcon = 'bottom.PNG';
+            stigmataColor = 11716965;
+          }
+          if (stigmataDB[indexes[0]].name !== '-') {
+            await msg.channel.send({
+              embed: {
+                author: {
+                  icon_url: `attachment://${authorIcon}`,
+                  name: stigmataDB[indexes[0]].name,
+                },
+                color: stigmataColor,
+                thumbnail: {
+                  url: stigmataDB[indexes[0]].image,
+                },
+                fields: [{
+                  name: 'Effect',
+                  value: stigmataDB[indexes[0]].skill,
+                }],
+              },
+              files: [{
+                attachment: `images/equipment/stigmata/${authorIcon}`,
+                name: authorIcon,
+              }],
+            });
+          }
+        } else if (result.length < 1) {
+          msg.reply('Stigmata not found!');
+        } else if (result.length > 1 && result.length < 4) {
+          stigmataSearchMenu(indexes);
+        }
+      }
     }
 
     if (msg.content.startsWith('!stigmata')) {
       if (msg.content === '!stigmata') {
         stigmataMenu();
+      } else {
+        stigmataSearch();
       }
     }
 
